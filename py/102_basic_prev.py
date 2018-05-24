c032d920d8ef191594dd3b76600056570134c41c
kazuki-onodera <kazuki.onodera@dena.com>
2018-05-24 19:58:57 +0900

    update

--- py/102_basic_prev.py
+++ py/102_basic_prev.py
@@ -11,6 +11,7 @@ previous_application
 
 import numpy as np
 import pandas as pd
+import gc
 import utils
 utils.start(__file__)
 #==============================================================================
@@ -18,16 +19,25 @@ utils.start(__file__)
 KEY = 'SK_ID_CURR'
 PREF = 'prev_'
 
+col_num = ['AMT_ANNUITY', 'AMT_APPLICATION', 'AMT_CREDIT', 'AMT_DOWN_PAYMENT', 
+           'AMT_GOODS_PRICE', 'FLAG_LAST_APPL_PER_CONTRACT', 'NFLAG_LAST_APPL_IN_DAY',
+           'RATE_DOWN_PAYMENT', 'RATE_INTEREST_PRIMARY', 'RATE_INTEREST_PRIVILEGED',
+           'DAYS_DECISION', 'DAYS_FIRST_DRAWING', 'DAYS_FIRST_DUE',
+           'DAYS_LAST_DUE_1ST_VERSION', 'DAYS_LAST_DUE', 'DAYS_TERMINATION',
+           'NFLAG_INSURED_ON_APPROVAL']
+
+col_cat = ['NAME_CONTRACT_TYPE', 'WEEKDAY_APPR_PROCESS_START',
+           'NAME_CASH_LOAN_PURPOSE', 'NAME_CONTRACT_STATUS', 'NAME_PAYMENT_TYPE',
+           'CODE_REJECT_REASON', 'NAME_TYPE_SUITE', 'NAME_CLIENT_TYPE',
+           'NAME_GOODS_CATEGORY', 'NAME_PORTFOLIO', 'NAME_PRODUCT_TYPE',
+           'CHANNEL_TYPE', 'NAME_SELLER_INDUSTRY', 'NAME_YIELD_GROUP', 'PRODUCT_COMBINATION']
 
 # =============================================================================
 # feature
 # =============================================================================
 prev = utils.read_pickles('../data/previous_application')
 
-# NAME_CONTRACT_TYPE
-df = pd.crosstab(prev[KEY], prev['NAME_CONTRACT_TYPE'])
-df.columns = ['prev_'+c.replace(' ', '-')+'_sum' for c in df.columns]
-
+base = prev[[KEY]].drop_duplicates().set_index(KEY)
 
 gr = prev.groupby(KEY)
 
@@ -35,47 +45,38 @@ def nunique(x):
     return len(set(x))
 
 # stats
-col_numeric = ['AMT_ANNUITY', 'AMT_APPLICATION', 'AMT_CREDIT', 'AMT_DOWN_PAYMENT', 
-               'AMT_GOODS_PRICE', 'FLAG_LAST_APPL_PER_CONTRACT', 'NFLAG_LAST_APPL_IN_DAY',
-               'RATE_DOWN_PAYMENT', 'RATE_INTEREST_PRIMARY', 'RATE_INTEREST_PRIVILEGED',
-               'DAYS_DECISION', 'DAYS_FIRST_DRAWING', 'DAYS_FIRST_DUE',
-               'DAYS_LAST_DUE_1ST_VERSION', 'DAYS_LAST_DUE', 'DAYS_TERMINATION',
-               'NFLAG_INSURED_ON_APPROVAL']
-for c in col_numeric:
+for c in col_num:
+    gc.collect()
     print(c)
-    df[f'{PREF}{c}_min'] = gr[c].min()
-    df[f'{PREF}{c}_max'] = gr[c].max()
-    df[f'{PREF}{c}_max-min'] = df[f'{PREF}{c}_max'] - df[f'{PREF}{c}_min']
-    df[f'{PREF}{c}_mean'] = gr[c].mean()
-    df[f'{PREF}{c}_std'] = gr[c].std()
-    df[f'{PREF}{c}_sum'] = gr[c].sum()
-    df[f'{PREF}{c}_nunique'] = gr[c].apply(nunique)
+    base[f'{PREF}{c}_min'] = gr[c].min()
+    base[f'{PREF}{c}_max'] = gr[c].max()
+    base[f'{PREF}{c}_max-min'] = base[f'{PREF}{c}_max'] - base[f'{PREF}{c}_min']
+    base[f'{PREF}{c}_mean'] = gr[c].mean()
+    base[f'{PREF}{c}_std'] = gr[c].std()
+    base[f'{PREF}{c}_sum'] = gr[c].sum()
+    base[f'{PREF}{c}_nunique'] = gr[c].apply(nunique)
     
 
-
-col_cat = ['NAME_CONTRACT_TYPE', 'WEEKDAY_APPR_PROCESS_START',
-           'NAME_CASH_LOAN_PURPOSE', 'NAME_CONTRACT_STATUS', 'NAME_PAYMENT_TYPE',
-           'CODE_REJECT_REASON', 'NAME_TYPE_SUITE', 'NAME_CLIENT_TYPE',
-           'NAME_GOODS_CATEGORY', 'NAME_PORTFOLIO', 'NAME_PRODUCT_TYPE',
-           'CHANNEL_TYPE', 'NAME_SELLER_INDUSTRY', 'NAME_YIELD_GROUP', 'PRODUCT_COMBINATION']
 for c in col_cat:
+    gc.collect()
     print(c)
-    df[f'{PREF}{c}_nunique'] = gr[c].apply(nunique)
-    
+    df = pd.crosstab(prev[KEY], prev[c])
+    df.columns = ['{PREF}'+c.replace(' ', '-')+'_sum' for c in df.columns]
+    base = pd.concat([base, df])
 
 
-df.reset_index(inplace=True)
+base.reset_index(inplace=True)
 
 # =============================================================================
 # merge
 # =============================================================================
 
 train = utils.load_train([KEY])
-train = pd.merge(train, df, on=KEY, how='left')
+train = pd.merge(train, base, on=KEY, how='left')
 
 
 test = utils.load_test([KEY])
-test = pd.merge(test, df, on=KEY, how='left')
+test = pd.merge(test, base, on=KEY, how='left')
 
 utils.to_pickles(train, '../data/102_train', utils.SPLIT_SIZE)
 utils.to_pickles(test,  '../data/102_test',  utils.SPLIT_SIZE)
