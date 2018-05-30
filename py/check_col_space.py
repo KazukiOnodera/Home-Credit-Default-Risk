b876ef4fdf7ef44dc91abd164979df7c204e1e75
kazuki-onodera <kazuki.onodera@dena.com>
2018-05-31 01:39:18 +0900

    update

--- py/check_col_space.py
+++ py/check_col_space.py
@@ -15,7 +15,7 @@ folders = glob('../data/*_train')
 def read(folder):
     df = pd.read_pickle(folder+'/000.p')
     for c in df.columns:
-        if 'index' == c:
+        if ' ' == c:
             print(folder)
             break
     return
