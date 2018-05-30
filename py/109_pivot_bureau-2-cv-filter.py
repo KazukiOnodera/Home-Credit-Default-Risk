4477fa1c5b92a5a879c7c49a229a0a037fccaeff
kazuki-onodera <kazuki.onodera@dena.com>
2018-05-30 23:05:36 +0900

    update

---
+++ py/109_pivot_bureau-2-cv-filter.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 30 22:31:33 2018
+
+@author: kazuki.onodera
+"""
+
+from glob import glob
+from os import system
+import pandas as pd
+import gc
+import sys
+sys.path.append('/home/kazuki_onodera/Python')
+import lgbmextension as ex
+import lightgbm as lgb
+import multiprocessing
+import utils
+utils.start(__file__)
+#==============================================================================
+
+SEED = 71
+
+
+FOLDERS = glob('../data/108*_train')
+FOLDERS += glob('../data/109*_train')
+
+X = pd.concat([
+               utils.read_pickles(f) for f in (FOLDERS)
+               ], axis=1)
+y = utils.read_pickles('../data/label').TARGET
+
+print(f'X.shape {X.shape}')
+
+param = {
+         'objective': 'binary',
+         'metric': 'auc',
+         'learning_rate': 0.01,
+         'max_depth': -1,
+         'num_leaves': 511,
+         'max_bin': 100,
+         'colsample_bytree': 0.1,
+         'subsample': 0.5,
+         'nthread': 63,
+         'bagging_freq': 1,
+         
+         'seed': SEED
+         }
+
+
+categorical_feature = ['NAME_CONTRACT_TYPE',
+                     'CODE_GENDER',
+                     'FLAG_OWN_CAR',
+                     'FLAG_OWN_REALTY',
+                     'NAME_TYPE_SUITE',
+                     'NAME_INCOME_TYPE',
+                     'NAME_EDUCATION_TYPE',
+                     'NAME_FAMILY_STATUS',
+                     'NAME_HOUSING_TYPE',
+                     'OCCUPATION_TYPE',
+                     'WEEKDAY_APPR_PROCESS_START',
+                     'ORGANIZATION_TYPE',
+                     'FONDKAPREMONT_MODE',
+                     'HOUSETYPE_MODE',
+                     'WALLSMATERIAL_MODE',
+                     'EMERGENCYSTATE_MODE']
+
+#dtrain = lgb.Dataset(X, y, categorical_feature=list( set(X.columns)&set(categorical_feature)) )
+#
+#ret = lgb.cv(param, dtrain, 9999, nfold=5,
+#             early_stopping_rounds=50, verbose_eval=10,
+#             seed=SEED)
+#print(f"CV auc-mean {ret['auc-mean'][-1]}")
+
+dtrain = lgb.Dataset(X, y, categorical_feature=list( set(X.columns)&set(categorical_feature)) )
+#model = lgb.train(param, dtrain, len(ret['auc-mean']))
+model = lgb.train(param, dtrain, 600)
+
+imp = ex.getImp(model)
+
+
+imp.to_csv(f'LOG/imp_{__file__}.csv', index=False)
+
+# =============================================================================
+# 
+# =============================================================================
+feature_all = imp[imp['split'] != 0].index.tolist()
+
+
+def read_pickle(folder, usecols):
+    df = pd.read_pickle(folder+'/000.p')
+    col = list( set(usecols) & set(df.columns))
+    if len(col)>0:
+        df = utils.read_pickles(folder, col)
+        utils.to_pickles(df, folder+'_filtered', utils.SPLIT_SIZE)
+        del df; gc.collect()
+        
+        folder = folder.replace('_train', '_test')
+        df = utils.read_pickles(folder, col)
+        utils.to_pickles(df, folder+'_filtered', utils.SPLIT_SIZE)
+        
+    else:
+        print(f'{folder} doesnt have valid features')
+        pass
+    
+
+[read_pickle(f, feature_all) for f in FOLDERS]
+
+#==============================================================================
+utils.end(__file__)
+
+
+
--- py/110_crostab-cat-comb_prev.py
+++ py/110_crostab-cat-comb_prev.py
@@ -8,7 +8,7 @@ Created on Wed May 30 22:01:36 2018
 import pandas as pd
 import gc
 from glob import glob
-from tqdm import tqdm
+from multiprocessing import Pool
 from itertools import combinations
 import utils
 utils.start(__file__)
@@ -16,13 +16,24 @@ utils.start(__file__)
 
 KEY = 'SK_ID_CURR'
 PREF = 'prev'
+NTHREAD = 15
 
-
-col_cat = ['NAME_CONTRACT_TYPE', 'WEEKDAY_APPR_PROCESS_START',
-           'NAME_CASH_LOAN_PURPOSE', 'NAME_CONTRACT_STATUS', 'NAME_PAYMENT_TYPE',
-           'CODE_REJECT_REASON', 'NAME_TYPE_SUITE', 'NAME_CLIENT_TYPE',
-           'NAME_GOODS_CATEGORY', 'NAME_PORTFOLIO', 'NAME_PRODUCT_TYPE',
-           'CHANNEL_TYPE', 'NAME_SELLER_INDUSTRY', 'NAME_YIELD_GROUP', 'PRODUCT_COMBINATION']
+col_cat = ['NAME_CONTRACT_TYPE', 
+           'FLAG_LAST_APPL_PER_CONTRACT',
+           'NFLAG_LAST_APPL_IN_DAY',
+           'NAME_CASH_LOAN_PURPOSE', 
+           'NAME_CONTRACT_STATUS', 
+           'NAME_PAYMENT_TYPE',
+           'CODE_REJECT_REASON', 
+           'NAME_TYPE_SUITE', 
+           'NAME_CLIENT_TYPE',
+           'NAME_GOODS_CATEGORY',
+           'NAME_PORTFOLIO', 
+           'NAME_PRODUCT_TYPE',
+           'CHANNEL_TYPE', 
+           'NAME_SELLER_INDUSTRY', 
+           'NAME_YIELD_GROUP', 
+           'PRODUCT_COMBINATION']
 
 
 col_cat_comb = list(combinations(col_cat, 2))
@@ -40,42 +51,41 @@ for cc in col_cat_comb:
     prev[f'{c1}-{c2}'] = prev[c1]+'-'+prev[c2]
     col_cat.append(f'{c1}-{c2}')
 
+train = utils.load_train([KEY])
+test = utils.load_test([KEY])
 
 # =============================================================================
 # cat
 # =============================================================================
-li = []
-col = []
-for c1 in col_cat:
-    gc.collect()
+
+def multi(c1):
+    global base
     print(c1)
     df_sum = pd.crosstab(prev[KEY], prev[c1])
     df_sum.columns = [f'{PREF}_{c1}_{str(c2).replace(" ", "-")}_sum' for c2 in df_sum.columns]
     df_norm = pd.crosstab(prev[KEY], prev[c1], normalize='index')
     df_norm.columns = [f'{PREF}_{c1}_{str(c2).replace(" ", "-")}_norm' for c2 in df_norm.columns]
     df = pd.concat([df_sum, df_norm], axis=1)
-    li.append(df)
-    col += df.columns.tolist()
-
-base = pd.concat([base]+li, axis=1)
-base[col] = base[col].fillna(-1)
-
+    
+    col = df.columns.tolist()
+    base = pd.concat([base, df], axis=1)
+    base[col] = base[col].fillna(-1)
+    base.reset_index(inplace=True)
+    
+    df = pd.merge(train, base, on=KEY, how='left').drop(KEY, axis=1)
+    utils.to_pickles(df, f'../data/110_{c1}_train', utils.SPLIT_SIZE)
+    gc.collect()
+    
+    df = pd.merge(test, base, on=KEY, how='left').drop(KEY, axis=1)
+    utils.to_pickles(df,  f'../data/110_{c1}_test',  utils.SPLIT_SIZE)
+    gc.collect()
 
 # =============================================================================
-# merge
+# 
 # =============================================================================
-base.reset_index(inplace=True)
-
-train = utils.load_train([KEY])
-train = pd.merge(train, base, on=KEY, how='left').drop(KEY, axis=1)
-
-
-test = utils.load_test([KEY])
-test = pd.merge(test, base, on=KEY, how='left').drop(KEY, axis=1)
-
-utils.to_pickles(train, '../data/110_train', utils.SPLIT_SIZE)
-utils.to_pickles(test,  '../data/110_test',  utils.SPLIT_SIZE)
-
+pool = Pool(NTHREAD)
+callback = pool.map(multi, col_cat)
+pool.close()
 
 
 #==============================================================================
