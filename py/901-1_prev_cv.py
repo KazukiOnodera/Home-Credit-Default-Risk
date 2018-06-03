3915d241551f80268280a46e427de7e00644cad5
Kazuki Onodera <luvsic02@gmail.com>
2018-06-04 01:25:50 +0900

    update

---
+++ py/901-1_prev_cv.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  4 01:12:35 2018
+
+@author: Kazuki
+"""
+
+import gc
+import pandas as pd
+import sys
+sys.path.append('/home/kazuki_onodera/Python')
+import lgbmextension as ex
+import lightgbm as lgb
+import multiprocessing
+from glob import glob
+import utils
+#utils.start(__file__)
+#==============================================================================
+
+SEED = 71
+
+folders = ['../data/101_train'] + sorted(glob('../data/102*_train_filtered'))
+folders += sorted(glob('../data/108*_train_filtered'))
+folders += ['../data/301_train']
+
+
+X = pd.concat([
+                utils.read_pickles(f) for f in (folders)
+               ], axis=1)
+y = utils.read_pickles('../data/label').TARGET
+
+
+if X.columns.duplicated().sum()>0:
+    raise Exception(f'duplicated!: { X.columns[X.columns.duplicated()] }')
+print('no dup :) ')
+print(f'X.shape {X.shape}')
+
+
+param = {
+         'objective': 'binary',
+         'metric': 'auc',
+         'learning_rate': 0.05,
+         'max_depth': -1,
+         'num_leaves': 127,
+         'max_bin': 127,
+         'colsample_bytree': 0.5,
+         'subsample': 0.5,
+         'nthread': multiprocessing.cpu_count(),
+         'bagging_freq': 1,
+#         'verbose':-1,
+         'seed': SEED
+         }
+
+
+categorical_feature = ['NAME_CONTRACT_TYPE',
+                     'CODE_GENDER',
+                     'FLAG_OWN_CAR',
+                     'FLAG_OWN_REALTY',
+                     'NAME_TYPE_SUITE',
+                     'NAME_INCOME_TYPE',
+                     'NAME_EDUCATION_TYPE',
+                     'NAME_FAMILY_STATUS',
+                     'NAME_HOUSING_TYPE',
+                     'OCCUPATION_TYPE',
+                     'WEEKDAY_APPR_PROCESS_START',
+                     'ORGANIZATION_TYPE',
+                     'FONDKAPREMONT_MODE',
+                     'HOUSETYPE_MODE',
+                     'WALLSMATERIAL_MODE',
+                     'EMERGENCYSTATE_MODE']
+
+
+
+dtrain = lgb.Dataset(X, y, categorical_feature=list( set(X.columns)&set(categorical_feature)) )
+
+ret = lgb.cv(param, dtrain, 9999, nfold=5,
+             early_stopping_rounds=50, verbose_eval=10,
+             seed=SEED)
+print(f"CV auc-mean {ret['auc-mean'][-1]}")
+
+
+dtrain = lgb.Dataset(X, y, categorical_feature=list( set(X.columns)&set(categorical_feature)) )
+model = lgb.train(param, dtrain, len(ret['auc-mean']))
+
+imp = ex.getImp(model)
+
+
+imp.to_csv(f'LOG/imp_{__file__}.csv', index=False)
+
+#==============================================================================
+utils.end(__file__)
+
+
+
