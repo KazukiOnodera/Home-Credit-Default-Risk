78b59bd41728a2ce0c40e682ee529e59f322fa4b
Kazuki Onodera <luvsic02@gmail.com>
2018-06-04 02:01:19 +0900

    update

---
+++ py/303-1_prev-CONTRACT_STATUS.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  4 00:40:34 2018
+
+@author: Kazuki
+"""
+
+import pandas as pd
+import gc
+from glob import glob
+from tqdm import tqdm
+import utils
+utils.start(__file__)
+#==============================================================================
+
+KEY = 'SK_ID_CURR'
+PREF = 'prev'
+
+
+col_cat = ['NAME_CONTRACT_TYPE', 'WEEKDAY_APPR_PROCESS_START',
+           'NAME_CASH_LOAN_PURPOSE', 'NAME_PAYMENT_TYPE',
+           'CODE_REJECT_REASON', 'NAME_TYPE_SUITE', 'NAME_CLIENT_TYPE',
+           'NAME_GOODS_CATEGORY', 'NAME_PORTFOLIO', 'NAME_PRODUCT_TYPE',
+           'CHANNEL_TYPE', 'NAME_SELLER_INDUSTRY', 'NAME_YIELD_GROUP', 'PRODUCT_COMBINATION']
+
+#col_group = ['SK_ID_PREV', 'NAME_CONTRACT_TYPE', 'WEEKDAY_APPR_PROCESS_START',
+#           'NAME_CASH_LOAN_PURPOSE', 'NAME_CONTRACT_STATUS', 'NAME_PAYMENT_TYPE',
+#           'CODE_REJECT_REASON', 'NAME_TYPE_SUITE', 'NAME_CLIENT_TYPE',
+#           'NAME_GOODS_CATEGORY', 'NAME_PORTFOLIO', 'NAME_PRODUCT_TYPE',
+#           'CHANNEL_TYPE', 'NAME_SELLER_INDUSTRY', 'NAME_YIELD_GROUP', 'PRODUCT_COMBINATION']
+
+# =============================================================================
+# feature
+# =============================================================================
+prev = utils.read_pickles('../data/previous_application')
+
+base = prev[[KEY]].drop_duplicates().set_index(KEY)
+
+values = prev['NAME_CONTRACT_STATUS'].unique()
+for v in values:
+    prev_ = prev[prev['NAME_CONTRACT_STATUS']==v]
+    PREF = f'prev_{v}'
+    # =============================================================================
+    # cat
+    # =============================================================================
+    for c1 in col_cat:
+        gc.collect()
+        print(c1)
+        df_sum = pd.crosstab(prev[KEY], prev[c1])
+        df_sum.columns = [f'{PREF}_{c1}_{str(c2).replace(" ", "-")}_sum' for c2 in df_sum.columns]
+        df_norm = pd.crosstab(prev[KEY], prev[c1], normalize='index')
+        df_norm.columns = [f'{PREF}_{c1}_{str(c2).replace(" ", "-")}_norm' for c2 in df_norm.columns]
+        df = pd.concat([df_sum, df_norm], axis=1)
+        col = df.columns.tolist()
+        base = pd.concat([base, df], axis=1)
+        base[col] = base[col].fillna(-1)
+
+
+# =============================================================================
+# merge
+# =============================================================================
+base.reset_index(inplace=True)
+
+train = utils.load_train([KEY])
+train = pd.merge(train, base, on=KEY, how='left').drop(KEY, axis=1)
+
+
+test = utils.load_test([KEY])
+test = pd.merge(test, base, on=KEY, how='left').drop(KEY, axis=1)
+
+utils.to_pickles(train, '../data/303-1_train', utils.SPLIT_SIZE)
+utils.to_pickles(test,  '../data/303-1_test',  utils.SPLIT_SIZE)
+
+
+
+#==============================================================================
+utils.end(__file__)
+
+
