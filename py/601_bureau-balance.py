16d2d02a3b701fbdb8d1f67ac85912ec9a009c7e
kazuki-onodera <kazuki.onodera@dena.com>
2018-06-04 20:40:07 +0900

    update

--- py/601_bureau-balance.py
+++ py/601_bureau-balance.py
@@ -123,8 +123,8 @@ train = pd.merge(train, base, on=KEY, how='left').drop(KEY, axis=1)
 test = utils.load_test([KEY])
 test = pd.merge(test, base, on=KEY, how='left').drop(KEY, axis=1)
 
-utils.to_pickles(train, '../data/107_train', utils.SPLIT_SIZE)
-utils.to_pickles(test,  '../data/107_test',  utils.SPLIT_SIZE)
+utils.to_pickles(train, '../data/601_train', utils.SPLIT_SIZE)
+utils.to_pickles(test,  '../data/601_test',  utils.SPLIT_SIZE)
 
 
 
