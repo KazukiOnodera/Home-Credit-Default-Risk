1e4e48a90deec493a9ae705bcd12d5af67eab371
Kazuki Onodera <luvsic02@gmail.com>
2018-06-06 09:38:10 +0900

    update

--- py/901-1_cv_606-2.py
+++ py/901-1_cv_606-2.py
@@ -16,7 +16,7 @@ import lightgbm as lgb
 import multiprocessing
 from glob import glob
 import utils
-#utils.start(__file__)
+utils.start(__file__)
 #==============================================================================
 
 SEED = 71
