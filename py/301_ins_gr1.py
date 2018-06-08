76f3156b4eec8921d1abf652032b229eac10140e
Kazuki Onodera <luvsic02@gmail.com>
2018-06-09 00:05:48 +0900

    update

--- py/301_ins_gr1.py
+++ py/301_ins_gr1.py
@@ -63,12 +63,13 @@ base.reset_index(inplace=True)
 
 train = utils.load_train([KEY])
 train = pd.merge(train, base, on=KEY, how='left').drop(KEY, axis=1)
-utils.to_pickles(train, f'../data/{No}_train', utils.SPLIT_SIZE)
+utils.to_pickle_each_cols(train, '../feature/train')
 del train; gc.collect()
+
 
 test = utils.load_test([KEY])
 test = pd.merge(test, base, on=KEY, how='left').drop(KEY, axis=1)
-utils.to_pickles(test,  f'../data/{No}_test',  utils.SPLIT_SIZE)
+utils.to_pickle_each_cols(test,  '../feature/test')
 
 
 
