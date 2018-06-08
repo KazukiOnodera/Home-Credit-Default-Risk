f9434fe9497f630db21b3ddceff6e987c2949780
Kazuki Onodera <luvsic02@gmail.com>
2018-06-08 23:53:54 +0900

    update

--- py/101_prev_gr1.py
+++ py/101_prev_gr1.py
@@ -79,13 +79,13 @@ base.reset_index(inplace=True)
 
 train = utils.load_train([KEY])
 train = pd.merge(train, base, on=KEY, how='left').drop(KEY, axis=1)
-utils.to_pickle_each_cols(train.add_prefix(PREF), '../feature/train')
+utils.to_pickle_each_cols(train, '../feature/train')
 del train; gc.collect()
 
 
 test = utils.load_test([KEY])
 test = pd.merge(test, base, on=KEY, how='left').drop(KEY, axis=1)
-utils.to_pickle_each_cols(test.add_prefix(PREF),  '../feature/test')
+utils.to_pickle_each_cols(test,  '../feature/test')
 
 
 
