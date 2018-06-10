97384ca87f799a431d3575951e099453518e67bf
Kazuki Onodera <luvsic02@gmail.com>
2018-06-10 18:08:03 +0900

    update

--- py/000.py
+++ py/000.py
@@ -59,21 +59,21 @@ df['days_update-end'] = df['DAYS_CREDIT_UPDATE'] - df['DAYS_CREDIT_ENDDATE']
 df['days_update-fact'] = df['DAYS_CREDIT_UPDATE'] - df['DAYS_ENDDATE_FACT']
 
 df['ant_cre-debt'] = df['AMT_CREDIT_SUM'] - df['AMT_CREDIT_SUM_DEBT']
-df['ant_cre-debt/limit'] = df['ant_cre-debt'] / df['AMT_CREDIT_SUM_LIMIT']
-df['ant_debt+limit'] = df['AMT_CREDIT_SUM_DEBT'] + df['AMT_CREDIT_SUM_LIMIT']
-df['ant_cre/debt+limit'] = df['AMT_CREDIT_SUM'] / df['ant_debt+limit']
+df['ant_cre-debt-by-limit'] = df['ant_cre-debt'] / df['AMT_CREDIT_SUM_LIMIT']
+df['ant_debt-p-limit'] = df['AMT_CREDIT_SUM_DEBT'] + df['AMT_CREDIT_SUM_LIMIT']
+df['ant_cre-by-debt-p-limit'] = df['AMT_CREDIT_SUM'] / df['ant_debt-p-limit']
 
-df['AMT_CREDIT_SUM/days_end-cre'] = df['AMT_CREDIT_SUM'] / df['days_end-cre']
-df['AMT_CREDIT_SUM/days_fact-cre'] = df['AMT_CREDIT_SUM'] / df['days_fact-cre']
-df['AMT_CREDIT_SUM/days_fact-end'] = df['AMT_CREDIT_SUM'] / df['days_fact-end']
-df['AMT_CREDIT_SUM/days_update-cre'] = df['AMT_CREDIT_SUM'] / df['days_update-cre']
-df['AMT_CREDIT_SUM/days_update-end'] = df['AMT_CREDIT_SUM'] / df['days_update-end']
+df['AMT_CREDIT_SUM-by-days_end-cre'] = df['AMT_CREDIT_SUM'] / df['days_end-cre']
+df['AMT_CREDIT_SUM-by-days_fact-cre'] = df['AMT_CREDIT_SUM'] / df['days_fact-cre']
+df['AMT_CREDIT_SUM-by-days_fact-end'] = df['AMT_CREDIT_SUM'] / df['days_fact-end']
+df['AMT_CREDIT_SUM-by-days_update-cre'] = df['AMT_CREDIT_SUM'] / df['days_update-cre']
+df['AMT_CREDIT_SUM-by-days_update-end'] = df['AMT_CREDIT_SUM'] / df['days_update-end']
 
 utils.to_pickles(df, '../data/bureau', utils.SPLIT_SIZE)
 
 
 # =============================================================================
-# 
+# bureau_balance
 # =============================================================================
 df = pd.read_csv('../input/bureau_balance.csv.zip')
 utils.to_pickles(df, '../data/bureau_balance', utils.SPLIT_SIZE)
---
+++ py/508_bureau_stats-consumer.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 10 18:02:09 2018
+
+@author: Kazuki
+"""
+
+import os
+import pandas as pd
+import gc
+from multiprocessing import Pool
+from glob import glob
+import utils
+utils.start(__file__)
+#==============================================================================
+KEY = 'SK_ID_CURR'
+No = '508'
+PREF = f'bureau_{No}'
+NTHREAD = 3
+
+
+# =============================================================================
+# feature
+# =============================================================================
+bureau = utils.read_pickles('../data/bureau')
+
+bureau = bureau[bureau['CREDIT_TYPE']=='Consumer credit']
+bureau = utils.get_dummies(bureau)
+
+base = bureau[[KEY]].drop_duplicates().set_index(KEY)
+
+
+def nunique(x):
+    return len(set(x))
+
+# =============================================================================
+# gr1
+# =============================================================================
+gr = bureau.groupby(KEY)
+
+# stats
+base[f'{PREF}_{KEY}_size'] = gr.size()
+
+base = pd.concat([
+                base,
+                gr.min().add_prefix(f'{PREF}_').add_suffix('_min'),
+                gr.max().add_prefix(f'{PREF}_').add_suffix('_max'),
+                gr.mean().add_prefix(f'{PREF}_').add_suffix('_mean'),
+                gr.std().add_prefix(f'{PREF}_').add_suffix('_std'),
+                gr.sum().add_prefix(f'{PREF}_').add_suffix('_sum'),
+                gr.quantile(0.25).add_prefix(f'{PREF}_').add_suffix('_q25'),
+                gr.quantile(0.50).add_prefix(f'{PREF}_').add_suffix('_q50'),
+                gr.quantile(0.75).add_prefix(f'{PREF}_').add_suffix('_q75'),
+                ], axis=1)
+
+
+
+# =============================================================================
+# merge
+# =============================================================================
+base.reset_index(inplace=True)
+
+train = utils.load_train([KEY])
+train = pd.merge(train, base, on=KEY, how='left').drop(KEY, axis=1)
+utils.to_pickle_each_cols(train, '../feature/train')
+del train; gc.collect()
+
+
+test = utils.load_test([KEY])
+test = pd.merge(test, base, on=KEY, how='left').drop(KEY, axis=1)
+utils.to_pickle_each_cols(test,  '../feature/test')
+
+
+
+#==============================================================================
+utils.end(__file__)
+
+
+
+
+
