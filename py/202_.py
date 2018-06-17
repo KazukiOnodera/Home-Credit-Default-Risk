ccf73d40bf6ec607e8206b4c65616edab858d93d
Kazuki Onodera <luvsic02@gmail.com>
2018-06-17 16:05:25 +0900

    update

--- py/202_.py
+++ py/202_.py
@@ -29,11 +29,12 @@ base = pos[[KEY]].drop_duplicates().set_index(KEY)
 gr_c   = pos.groupby('SK_ID_CURR')
 gr_cp  = pos.groupby(['SK_ID_CURR', 'SK_ID_PREV'])
 gr_cm  = pos.groupby(['SK_ID_CURR', 'MONTHS_BALANCE'])
+gr_ci  = pos.groupby(['SK_ID_CURR', 'CNT_INSTALMENT'])
+gr_cs  = pos.groupby(['SK_ID_CURR', 'NAME_CONTRACT_STATUS'])
 
 # =============================================================================
-# 
-# =============================================================================
 # size feature
+# =============================================================================
 base['c_size'] = gr_c.size()
 
 cp_size = gr_cp.size().groupby('SK_ID_CURR')
@@ -44,20 +45,43 @@ base['cp_size_max-min'] = base['cp_size_max'] - base['cp_size_min']
 base['p_nunique'] = cp_size.size()
 
 
-gr_cm.size().max()
+cm_size = gr_cm.size().groupby('SK_ID_CURR')
+base['cm_size_min']  = cm_size.min() # all1 ?
+base['cm_size_mean'] = cm_size.mean()
+base['cm_size_max']  = cm_size.max()
+base['cm_size_max-min'] = base['cm_size_max'] - base['cm_size_min']
+base['m_nunique'] = cm_size.size()
+
+
+ci_size = gr_ci.size().groupby('SK_ID_CURR')
+base['ci_size_min']  = ci_size.min() # all1 ?
+base['ci_size_mean'] = ci_size.mean()
+base['ci_size_max']  = ci_size.max()
+base['ci_size_max-min'] = base['ci_size_max'] - base['ci_size_min']
+base['i_nunique'] = ci_size.size()
+
+cs_size = gr_cs.size().groupby('SK_ID_CURR')
+base['cs_size_min']  = cs_size.min() # all1 ?
+base['cs_size_mean'] = cs_size.mean()
+base['cs_size_max']  = cs_size.max()
+base['cs_size_max-min'] = base['cs_size_max'] - base['cs_size_min']
+base['s_nunique'] = cs_size.size()
+
+# =============================================================================
+# NAME_CONTRACT_STATUS
+# =============================================================================
+
+ct1 = pd.crosstab(pos[KEY], pos['NAME_CONTRACT_STATUS'])
+ct2 = pd.crosstab(pos[KEY], pos['NAME_CONTRACT_STATUS'], normalize='index')
+
+base = pd.concat([base, ct1, ct2], axis=1)
 
 
 
 
 
 
-
-
-
-
-
-
-
+# TODO: DPD
 
 
 
@@ -85,16 +109,17 @@ gr_cm.size().max()
 # =============================================================================
 # merge
 # =============================================================================
+base.reset_index(inplace=True)
 
 train = utils.load_train([KEY])
 
 test = utils.load_test([KEY])
 
 
-train_ = pd.merge(train, pt, on=KEY, how='left').drop(KEY, axis=1)
+train_ = pd.merge(train, base, on=KEY, how='left').drop(KEY, axis=1)
 utils.to_feature(train_, '../feature/train')
 
-test_ = pd.merge(test, pt, on=KEY, how='left').drop(KEY, axis=1)
+test_ = pd.merge(test, base, on=KEY, how='left').drop(KEY, axis=1)
 utils.to_feature(test_,  '../feature/test')
 
 
