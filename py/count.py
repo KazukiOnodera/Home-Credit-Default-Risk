9087ab98ee4795256e9db1d15551374bd1f7417f
KazukiOnodera <luvsic02@gmail.com>
2018-06-18 11:35:29 +0900

    update

--- py/count.py
+++ py/count.py
@@ -10,7 +10,11 @@ from glob import glob
 import sys
 argv = sys.argv
 
-files = glob(f'../feature/{argv[1]}*.f')
-
-print(len(files))
-
+if len(argv)>1:
+    files = glob(f'../feature/{argv[1]}*.f')
+    print(len(files))
+    
+else:
+    files = glob(f'../feature/*.f')
+    files = sorted(list( set([f.split('/')[-1].split('_')[2] for f in files]) ))
+    
