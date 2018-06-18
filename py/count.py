e54e95dcb18d72ac28317c737376ca9352efdd25
KazukiOnodera <luvsic02@gmail.com>
2018-06-18 11:49:06 +0900

    update

--- py/count.py
+++ py/count.py
@@ -7,6 +7,7 @@ Created on Sun Jun 17 13:18:55 2018
 """
 
 from glob import glob
+from collections import defaultdict
 import sys
 argv = sys.argv
 
@@ -16,6 +17,18 @@ if len(argv)>1:
     
 else:
     files = glob(f'../feature/*.f')
-    files = sorted(list( set([f.split('/')[-1].split('_')[2] for f in files]) ))
-    [print(f) for f in files]
+    
+    keys = [f.split('/')[-1].split('_')[2] for f in files if 'train_' in f]
+    di = defaultdict(int)
+    for k in keys:
+        di[k] += 1
+    for k,v in di.items():
+        print(k, v)
+    
+    keys = [f.split('/')[-1].split('_')[2] for f in files if 'test_' in f]
+    di = defaultdict(int)
+    for k in keys:
+        di[k] += 1
+    for k,v in di.items():
+        print(k, v)
     
