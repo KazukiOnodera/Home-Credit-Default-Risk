93f1d9d21caae5efde5310895e00a41e6d0035b4
Kazuki Onodera <luvsic02@gmail.com>
2018-06-20 00:47:41 +0900

    update

---
+++ py/301_aggregation.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 19 23:14:19 2018
+
+@author: Kazuki
+
+based on
+https://www.kaggle.com/jsaguiar/updated-0-792-lb-lightgbm-with-simple-features/code
+"""
+
+import numpy as np
+import pandas as pd
+import gc
+import os
+from multiprocessing import Pool, cpu_count
+NTHREAD = cpu_count()
+import utils
+utils.start(__file__)
+#==============================================================================
+PREF = 'ins_301_
+
+KEY = 'SK_ID_CURR'
+
+day_start = -365*10 # min: -2922
+day_end   = -365*0  # min: -2922
+
+os.system(f'rm ../feature/t*_{PREF}*')
+# =============================================================================
+# 
+# =============================================================================
+ins = utils.read_pickles('../data/installments_payments')
+ins = ins[ins['DAYS_INSTALMENT'].between(day_start, day_end)]
+
+
+num_aggregations = {
+    # TODO: optimize stats
+    'NUM_INSTALMENT_VERSION': ['nunique'],
+    'DPD': ['max', 'mean', 'sum', 'nunique'],
+    'DBD': ['max', 'mean', 'sum', 'nunique'],
+    'amt_ratio': ['min', 'mean', 'var'],
+    'amt_delta': ['min', 'mean', 'var'],
+    'AMT_INSTALMENT': ['min', 'max', 'mean', 'sum'],
+    'AMT_PAYMENT':    ['min', 'max', 'mean', 'sum'],
+    'DAYS_ENTRY_PAYMENT': ['max', 'mean', 'sum']
+}
+
+#col_cat = ['NAME_CONTRACT_STATUS']
+
+train = utils.load_train([KEY])
+test = utils.load_test([KEY])
+
+# =============================================================================
+# 
+# =============================================================================
+def aggregate():
+    
+    df = ins
+    
+#    li = []
+#    for c1 in df.columns:
+#        for c2 in col_cat:
+#            if c1.startswith(c2+'_'):
+#                li.append(c1)
+#                break
+#    
+#    cat_aggregations = {}
+#    for cat in li:
+#        cat_aggregations[cat] = ['mean', 'sum']
+    
+    df_agg = df.groupby('SK_ID_CURR').agg({**num_aggregations, **cat_aggregations})
+    df_agg.columns = pd.Index([e[0] + "_" + e[1] for e in df_agg.columns.tolist()])
+    
+    df_agg['INS_COUNT'] = df.groupby('SK_ID_CURR').size()
+    df_agg.reset_index(inplace=True)
+    
+    tmp = pd.merge(train, df_agg, on=KEY, how='left').drop(KEY, axis=1)
+    utils.to_feature(tmp.add_prefix(PREF), '../feature/train')
+    
+    tmp = pd.merge(test, df_agg, on=KEY, how='left').drop(KEY, axis=1)
+    utils.to_feature(tmp.add_prefix(PREF),  '../feature/test')
+    
+    return
+
+
+# =============================================================================
+# main
+# =============================================================================
+
+aggregate()
+
+
+
+#==============================================================================
+utils.end(__file__)
