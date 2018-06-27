d6c2e5f94987939fe797bd2d93d98dd066f45ee9
Kazuki Onodera <luvsic02@gmail.com>
2018-06-27 23:22:46 +0900

    update

---
+++ py/901_cv_627-1.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 27 22:56:40 2018
+
+@author: Kazuki
+"""
+
+
+import gc
+from tqdm import tqdm
+import pandas as pd
+import sys
+sys.path.append('/home/kazuki_onodera/Python')
+import lgbmextension as ex
+import lightgbm as lgb
+import multiprocessing
+from glob import glob
+import utils
+utils.start(__file__)
+#==============================================================================
+
+# setting
+
+SEED = 71
+
+#imp_file = 'LOG/imp_901_cv_611-1.py.csv'
+imp_file = None
+
+param = {
+         'objective': 'binary',
+         'metric': 'auc',
+         'learning_rate': 0.01,
+         'max_depth': -1,
+         'num_leaves': 255,
+         'max_bin': 255,
+         'colsample_bytree': 0.9,
+         'subsample': 0.9,
+         'nthread': multiprocessing.cpu_count(),
+         'bagging_freq': 1,
+#         'verbose':-1,
+         'seed': SEED
+         }
+
+
+categorical_feature = ['app_001_NAME_CONTRACT_TYPE',
+                     'app_001_CODE_GENDER',
+                     'app_001_FLAG_OWN_CAR',
+                     'app_001_FLAG_OWN_REALTY',
+                     'app_001_NAME_TYPE_SUITE',
+                     'app_001_NAME_INCOME_TYPE',
+                     'app_001_NAME_EDUCATION_TYPE',
+                     'app_001_NAME_FAMILY_STATUS',
+                     'app_001_NAME_HOUSING_TYPE',
+                     'app_001_OCCUPATION_TYPE',
+                     'app_001_WEEKDAY_APPR_PROCESS_START',
+                     'app_001_ORGANIZATION_TYPE',
+                     'app_001_FONDKAPREMONT_MODE',
+                     'app_001_HOUSETYPE_MODE',
+                     'app_001_WALLSMATERIAL_MODE',
+                     'app_001_EMERGENCYSTATE_MODE']
+
+use_files = []
+
+# =============================================================================
+# set features
+# =============================================================================
+
+files = sorted(glob('../feature/train*.f'))
+
+unuse_files = [f.split('/')[-1] for f in sorted(glob('../unuse_feature/*.f'))]
+if len(unuse_files)>0:
+    files_ = []
+    for f1 in files:
+        for f2 in unuse_files:
+            if f1.endswith(f2):
+                files_.append(f1)
+                break
+
+    files = sorted(set(files) - set(files_))
+
+if len(use_files)>0:
+    files_ = []
+    for f1 in files:
+        for f2 in use_files:
+            if f2 in f1:
+                files_.append(f1)
+                break
+
+    files = sorted(files_[:])
+
+
+X = pd.concat([
+                pd.read_feather(f) for f in tqdm(files, mininterval=60)
+               ], axis=1)
+y = utils.read_pickles('../data/label').TARGET
+
+
+if X.columns.duplicated().sum()>0:
+    raise Exception(f'duplicated!: { X.columns[X.columns.duplicated()] }')
+print('no dup :) ')
+print(f'X.shape {X.shape}')
+
+
+
+# =============================================================================
+# CV
+# =============================================================================
+dtrain = lgb.Dataset(X, y, categorical_feature=list( set(X.columns)&set(categorical_feature)) )
+gc.collect()
+
+ret = lgb.cv(param, dtrain, 9999, nfold=5,
+             early_stopping_rounds=50, verbose_eval=10,
+             seed=SEED)
+print(f"CV auc-mean {ret['auc-mean'][-1]}")
+
+best_score = ret['auc-mean'][-1]
+utils.send_line(f'all features best_score: {best_score}')
+
+# =============================================================================
+# 
+# =============================================================================
+dtrain = lgb.Dataset(X, y, categorical_feature=list( set(X.columns)&set(categorical_feature)) )
+model = lgb.train(param, dtrain, len(ret['auc-mean']))
+
+imp = ex.getImp(model)
+
+
+imp.to_csv(f'LOG/imp_{__file__}.csv', index=False)
+
+#==============================================================================
+utils.end(__file__)
+
