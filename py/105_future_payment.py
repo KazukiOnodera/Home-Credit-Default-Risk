fbb8389137d011c3f6f2152bf99b30ff996de1fa
Kazuki Onodera <luvsic02@gmail.com>
2018-07-02 11:06:26 +0900

    update

--- py/105_future_payment.py
+++ py/105_future_payment.py
@@ -10,7 +10,7 @@ import numpy as np
 import pandas as pd
 import os
 import utils
-#utils.start(__file__)
+utils.start(__file__)
 #==============================================================================
 
 PREF = 'prev_105_'
