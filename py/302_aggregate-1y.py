7bc285e8cc8bcab9fc9b195331f97b8eed7aa721
Kazuki Onodera <luvsic02@gmail.com>
2018-07-06 08:27:39 +0900

    update

--- py/302_aggregate-1y.py
+++ py/302_aggregate-1y.py
@@ -13,8 +13,8 @@ import numpy as np
 import pandas as pd
 import gc
 import os
-from multiprocessing import Pool, cpu_count
-NTHREAD = cpu_count()
+#from multiprocessing import Pool, cpu_count
+#NTHREAD = cpu_count()
 import utils_agg
 import utils
 utils.start(__file__)
@@ -35,11 +35,9 @@ ins = ins[ins['DAYS_INSTALMENT'].between(day_start, day_end)]
 
 ins_d = utils.read_pickles('../data/installments_payments_delay')
 ins_d = ins_d[ins_d['DAYS_INSTALMENT'].between(day_start, day_end)]
-ins_d.columns = ins_d.columns[:2].tolist() + ('delay_' + ins_d.columns[2:]).tolist()
 
 ins_nd = utils.read_pickles('../data/installments_payments_notdelay')
 ins_nd = ins_nd[ins_nd['DAYS_INSTALMENT'].between(day_start, day_end)]
-ins_nd.columns = ins_nd.columns[:2].tolist() + ('notdelay_' + ins_nd.columns[2:]).tolist()
 
 
 #col_cat = ['NAME_CONTRACT_STATUS']
@@ -52,6 +50,7 @@ test = utils.load_test([KEY])
 # =============================================================================
 def aggregate(df, pref):
     
+    del df['SK_ID_PREV']
     df_agg = df.groupby('SK_ID_CURR').agg({**utils_agg.ins_num_aggregations})
     
 #    gr1 = df.groupby(['SK_ID_PREV', 'SK_ID_CURR', 'NUM_INSTALMENT_NUMBER'])
@@ -59,8 +58,8 @@ def aggregate(df, pref):
     
     df_agg.columns = pd.Index([e[0] + "_" + e[1] for e in df_agg.columns.tolist()])
     
-    df_agg[pref + 'INS_COUNT'] = df.groupby('SK_ID_CURR').size()
-    df_agg.reset_index(inplace=True)
+    df_agg['INS_COUNT'] = df.groupby('SK_ID_CURR').size()
+    df_agg = df_agg.add_prefix(pref).reset_index()
     
     tmp = pd.merge(train, df_agg, on=KEY, how='left').drop(KEY, axis=1)
     utils.to_feature(tmp.add_prefix(PREF), '../feature/train')
@@ -69,6 +68,7 @@ def aggregate(df, pref):
     utils.to_feature(tmp.add_prefix(PREF),  '../feature/test')
     
     return
+
 
 
 # =============================================================================
