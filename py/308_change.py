118f195bf208550b55b1944de190c87a91c634ce
KazukiOnodera <luvsic02@gmail.com>
2018-07-20 20:14:15 +0900

    update

--- py/308_change.py
+++ py/308_change.py
@@ -35,8 +35,21 @@ train = utils.load_train([KEY])
 test = utils.load_test([KEY])
 prev = utils.read_pickles('../data/previous_application', ['SK_ID_PREV', 'NAME_CONTRACT_TYPE'])
 
-COL = ['AMT_PAYMENT', 'AMT_PAYMENT-d-app_AMT_INCOME_TOTAL', 'AMT_PAYMENT-d-app_AMT_CREDIT',
-       'AMT_PAYMENT-d-app_AMT_ANNUITY', 'AMT_PAYMENT-d-app_AMT_GOODS_PRICE']
+COL = ['NUM_INSTALMENT_VERSION',
+       'NUM_INSTALMENT_NUMBER', 'DAYS_INSTALMENT', 
+       'AMT_INSTALMENT', 'AMT_PAYMENT', 'AMT_ANNUITY',
+       'CNT_PAYMENT', 
+#       'DAYS_ENTRY_PAYMENT',
+#       'DAYS_ENTRY_PAYMENT-m-app_DAYS_BIRTH',
+#       'DAYS_ENTRY_PAYMENT-m-app_DAYS_EMPLOYED',
+#       'DAYS_ENTRY_PAYMENT-m-app_DAYS_REGISTRATION',
+#       'DAYS_ENTRY_PAYMENT-m-app_DAYS_ID_PUBLISH',
+#       'DAYS_ENTRY_PAYMENT-m-app_DAYS_LAST_PHONE_CHANGE',
+       'AMT_PAYMENT-d-app_AMT_INCOME_TOTAL', 'AMT_PAYMENT-d-app_AMT_CREDIT',
+       'AMT_PAYMENT-d-app_AMT_ANNUITY', 'AMT_PAYMENT-d-app_AMT_GOODS_PRICE',
+       'NUM_INSTALMENT_ratio', 'AMT_PAYMENT-d-AMT_ANNUITY',
+       'days_delayed_payment', 'amt_ratio', 'amt_delta', 'days_weighted_delay',
+       'DPD', 'DBD', 'days_weighted_delay_tsw3']
 # =============================================================================
 # 
 # =============================================================================
@@ -52,7 +65,7 @@ def aggregate(args):
     path, cont_type, pref = args
     
     df = utils.read_pickles(path, [KEY, 'SK_ID_PREV', 'DAYS_ENTRY_PAYMENT']+COL)
-    df = df[df['DAYS_ENTRY_PAYMENT'].between(day_start, day_end)].sort_values([KEY, 'DAYS_ENTRY_PAYMENT'])
+#    df = df[df['DAYS_ENTRY_PAYMENT'].between(day_start, day_end)].sort_values([KEY, 'DAYS_ENTRY_PAYMENT'])
     df = pd.merge(df, prev, on='SK_ID_PREV', how='left'); gc.collect()
     
     if cont_type=='NA':
@@ -61,20 +74,41 @@ def aggregate(args):
         df = df[df['NAME_CONTRACT_TYPE']==cont_type]
     
     df.sort_values(['SK_ID_PREV', 'DAYS_ENTRY_PAYMENT'], inplace=True)
+    df.reset_index(drop=True, inplace=True)
     
-    col_ = []
+    li = []
     for c in COL:
-        df[f'{c}_diff'] = df[['SK_ID_PREV', c]].groupby('SK_ID_PREV')[c].diff()
-        df[f'{c}_diff_diff'] = df[['SK_ID_PREV', f'{c}_diff']].groupby('SK_ID_PREV')[f'{c}_diff'].diff()
-        df[f'{c}_pctchange'] = df[['SK_ID_PREV', c]].groupby('SK_ID_PREV')[c].pct_change()
-        df[f'{c}_pctchange_pctchange'] = df[['SK_ID_PREV', f'{c}_pctchange']].groupby('SK_ID_PREV')[f'{c}_pctchange'].pct_change()
-        col_ += [f'{c}_diff', f'{c}_diff_diff', f'{c}_pctchange', f'{c}_pctchange_pctchange']
+        ret_diff = []
+        ret_pctchng = []
+        key_bk = x_bk = None
+        for key, x in df[['SK_ID_PREV', c]].values:
+            
+            if key_bk is None:
+                ret_diff.append(None)
+                ret_pctchng.append(None)
+            else:
+                if key_bk == key:
+                    ret_diff.append(x - x_bk)
+                    ret_pctchng.append( (x_bk-x) / x_bk)
+                else:
+                    ret_diff.append(None)
+                    ret_pctchng.append(None)
+            key_bk = key
+            x_bk = x
+            
+        ret_diff = pd.Series(ret_diff, name=f'{c}_diff')
+        ret_pctchng = pd.Series(ret_pctchng, name=f'{c}_pctchange')
+        ret = pd.concat([ret_diff, ret_pctchng], axis=1)
+        li.append(ret)
+    callback = pd.concat(li, axis=1)
+    col_ = callback.columns.tolist()
+    callback[KEY] = df[KEY]
     
     num_agg = {}
     for c in col_:
         num_agg[c] = ['min', 'mean', 'max', 'var']
     
-    feature = df.groupby(KEY).agg(**num_agg)
+    feature = callback.groupby(KEY).agg(num_agg)
     feature.columns = pd.Index([e[0] + "_" + e[1] for e in feature.columns.tolist()])
     feature.reset_index(inplace=True)
     
