cd3a5d311156c5f13ba97c446e20efd07e17c433
Kazuki Onodera <luvsic02@gmail.com>
2018-07-23 02:06:06 +0900

    update

--- py/806_cv_best.py
+++ py/806_cv_best.py
@@ -66,7 +66,7 @@ CAT = list( set(X.columns)&set(utils_cat.ALL) )
 # cv
 # =============================================================================
 dtrain = lgb.Dataset(X, y, categorical_feature=CAT ,
-                     weight=np.array(weight))
+                     weight=weight.values)
 gc.collect()
 
 ret = lgb.cv(param, dtrain, 9999, nfold=7,
