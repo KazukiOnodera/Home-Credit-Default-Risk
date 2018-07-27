d212635ccec4451061b64de0d622a7e55178aa69
Kazuki Onodera <luvsic02@gmail.com>
2018-07-27 06:41:13 +0000

    update

---
+++ jn/utils_cat.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 11 11:21:27 2018
+
+@author: kazuki.onodera
+"""
+
+
+# =============================================================================
+# 
+# =============================================================================
+
+f002 = ['f002_NAME_CONTRACT_TYPE',
+         'f002_CODE_GENDER',
+         'f002_FLAG_OWN_CAR',
+         'f002_FLAG_OWN_REALTY',
+         'f002_NAME_TYPE_SUITE',
+         'f002_NAME_INCOME_TYPE',
+         'f002_NAME_EDUCATION_TYPE',
+         'f002_NAME_FAMILY_STATUS',
+         'f002_NAME_HOUSING_TYPE',
+         'f002_OCCUPATION_TYPE',
+         'f002_WEEKDAY_APPR_PROCESS_START',
+         'f002_ORGANIZATION_TYPE',
+         'f002_FONDKAPREMONT_MODE',
+         'f002_HOUSETYPE_MODE',
+         'f002_WALLSMATERIAL_MODE',
+         'f002_EMERGENCYSTATE_MODE']
+
+
+
+# =============================================================================
+# 
+# =============================================================================
+
+f108 = ['f108_NAME_CONTRACT_TYPE',
+                     'f108_WEEKDAY_APPR_PROCESS_START',
+                     'f108_NAME_CASH_LOAN_PURPOSE',
+                     'f108_NAME_CONTRACT_STATUS',
+                     'f108_NAME_PAYMENT_TYPE',
+                     'f108_CODE_REJECT_REASON',
+                     'f108_NAME_TYPE_SUITE',
+                     'f108_NAME_CLIENT_TYPE',
+                     'f108_NAME_GOODS_CATEGORY',
+                     'f108_NAME_PORTFOLIO',
+                     'f108_NAME_PRODUCT_TYPE',
+                     'f108_CHANNEL_TYPE',
+                     'f108_NAME_SELLER_INDUSTRY',
+                     'f108_NAME_YIELD_GROUP',
+                     'f108_PRODUCT_COMBINATION']
+
+
+f109 = ['f109_NAME_CONTRACT_TYPE',
+         'f109_WEEKDAY_APPR_PROCESS_START',
+         'f109_NAME_CASH_LOAN_PURPOSE',
+         'f109_NAME_CONTRACT_STATUS',
+         'f109_NAME_PAYMENT_TYPE',
+         'f109_CODE_REJECT_REASON',
+         'f109_NAME_TYPE_SUITE',
+         'f109_NAME_CLIENT_TYPE',
+         'f109_NAME_GOODS_CATEGORY',
+         'f109_NAME_PORTFOLIO',
+         'f109_NAME_PRODUCT_TYPE',
+         'f109_CHANNEL_TYPE',
+         'f109_NAME_SELLER_INDUSTRY',
+         'f109_NAME_YIELD_GROUP',
+         'f109_PRODUCT_COMBINATION']
+
+
+# =============================================================================
+# 
+# =============================================================================
+
+f509 = ['f509_CREDIT_ACTIVE', 'f509_CREDIT_CURRENCY', 'f509_CREDIT_TYPE']
+f510 = ['f510_CREDIT_ACTIVE', 'f510_CREDIT_CURRENCY', 'f510_CREDIT_TYPE']
+
+
+# =============================================================================
+# 
+# =============================================================================
+maxwell = ['Mxw_FLAG_PHONE_PATTERN', 'Mxw_FLAG_DOC_PATTERN']
+
+
+
+# =============================================================================
+# all
+# =============================================================================
+
+ALL = f002 + f108 + f109 + f509 + f510 + maxwell
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
