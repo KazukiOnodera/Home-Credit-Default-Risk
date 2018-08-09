1ba9c6597a38ea104773f3cb282843e791977fa7
Kazuki Onodera <luvsic02@gmail.com>
2018-08-10 06:45:19 +0900

    update

--- py/730_pure_gp.py
+++ py/730_pure_gp.py
@@ -1681,10 +1681,12 @@ def bureau_and_balance(num_rows = None, nan_as_category = True):
     
     bureau_agg = bureau.groupby('SK_ID_CURR').agg({**num_aggregations, **cat_aggregations})
     bureau_agg.columns = pd.Index(['BURO_' + e[0] + "_" + e[1].upper() for e in bureau_agg.columns.tolist()])
+    bureau_agg.reset_index(inplace=True)
     # Bureau: Active credits - using only numerical aggregations
     active = bureau[bureau['CREDIT_ACTIVE_Active'] == 1]
     active_agg = active.groupby('SK_ID_CURR').agg(num_aggregations)
     active_agg.columns = pd.Index(['ACTIVE_' + e[0] + "_" + e[1].upper() for e in active_agg.columns.tolist()])
+    active_agg.reset_index(inplace=True)
     bureau_agg = bureau_agg.join(active_agg, how='left', on='SK_ID_CURR')
     del active, active_agg
     gc.collect()
@@ -1692,6 +1694,7 @@ def bureau_and_balance(num_rows = None, nan_as_category = True):
     closed = bureau[bureau['CREDIT_ACTIVE_Closed'] == 1]
     closed_agg = closed.groupby('SK_ID_CURR').agg(num_aggregations)
     closed_agg.columns = pd.Index(['CLOSED_' + e[0] + "_" + e[1].upper() for e in closed_agg.columns.tolist()])
+    closed_agg.reset_index(inplace=True)
     bureau_agg = bureau_agg.join(closed_agg, how='left', on='SK_ID_CURR')
     del closed, closed_agg, bureau
     gc.collect()
@@ -1729,15 +1732,18 @@ def previous_applications(num_rows = None, nan_as_category = True):
     
     prev_agg = prev.groupby('SK_ID_CURR').agg({**num_aggregations, **cat_aggregations})
     prev_agg.columns = pd.Index(['PREV_' + e[0] + "_" + e[1].upper() for e in prev_agg.columns.tolist()])
+    prev_agg.reset_index(inplace=True)
     # Previous Applications: Approved Applications - only numerical features
     approved = prev[prev['NAME_CONTRACT_STATUS_Approved'] == 1]
     approved_agg = approved.groupby('SK_ID_CURR').agg(num_aggregations)
     approved_agg.columns = pd.Index(['APPROVED_' + e[0] + "_" + e[1].upper() for e in approved_agg.columns.tolist()])
+    approved_agg.reset_index(inplace=True)
     prev_agg = prev_agg.join(approved_agg, how='left', on='SK_ID_CURR')
     # Previous Applications: Refused Applications - only numerical features
     refused = prev[prev['NAME_CONTRACT_STATUS_Refused'] == 1]
     refused_agg = refused.groupby('SK_ID_CURR').agg(num_aggregations)
     refused_agg.columns = pd.Index(['REFUSED_' + e[0] + "_" + e[1].upper() for e in refused_agg.columns.tolist()])
+    refused_agg.reset_index(inplace=True)
     prev_agg = prev_agg.join(refused_agg, how='left', on='SK_ID_CURR')
     del refused, refused_agg, approved, approved_agg, prev
     gc.collect()
@@ -1827,19 +1833,19 @@ with timer("Process previous_applications"):
     del prev
     gc.collect()
 with timer("Process POS-CASH balance"):
-    pos = pos_cash(num_rows)
+    pos = pos_cash(num_rows).reset_index()
     print("Pos-cash balance df shape:", pos.shape)
     df = df.join(pos, how='left', on='SK_ID_CURR')
     del pos
     gc.collect()
 with timer("Process installments payments"):
-    ins = installments_payments(num_rows)
+    ins = installments_payments(num_rows).reset_index()
     print("Installments payments df shape:", ins.shape)
     df = df.join(ins, how='left', on='SK_ID_CURR')
     del ins
     gc.collect()
 with timer("Process credit card balance"):
-    cc = credit_card_balance(num_rows)
+    cc = credit_card_balance(num_rows).reset_index()
     print("Credit card balance df shape:", cc.shape)
     df = df.join(cc, how='left', on='SK_ID_CURR')
     del cc
