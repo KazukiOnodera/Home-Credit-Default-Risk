f93c716cc4a8635a1543e0e5bbe3a7dfb748a378
Kazuki Onodera <luvsic02@gmail.com>
2018-08-12 15:51:19 +0900

    update

---
+++ py/815_imp_lgb_loop.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 12 15:33:01 2018
+
+@author: Kazuki
+"""
+
+
+import gc, os
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
+import sys
+#argv = sys.argv
+sys.path.append(f'/home/{os.environ.get("USER")}/PythonLibrary')
+import lgbextension as ex
+import lightgbm as lgb
+from multiprocessing import cpu_count
+#from collections import defaultdict
+#from glob import glob
+#import count
+import utils, utils_cat
+utils.start(__file__)
+#==============================================================================
+
+#SEED = int(argv[1])
+
+
+param = {
+         'objective': 'binary',
+         'metric': 'auc',
+         'learning_rate': 0.01,
+         'max_depth': 6,
+         'num_leaves': 63,
+         'max_bin': 255,
+         
+         'min_child_weight': 10,
+         'min_data_in_leaf': 150,
+         'reg_lambda': 0.5,  # L2 regularization term on weights.
+         'reg_alpha': 0.5,  # L1 regularization term on weights.
+         
+         'colsample_bytree': 0.9,
+         'subsample': 0.9,
+#         'nthread': 32,
+         'nthread': cpu_count(),
+         'bagging_freq': 1,
+         'verbose':-1,
+#         'seed': SEED
+         }
+
+
+
+imp = pd.read_csv('LOG/imp_801_imp_lgb.py_s111_s72.csv')
+imp.sort_values('total', ascending=False, inplace=True)
+
+files = ('../feature/train_' + imp.head(3000).feature + '.f').tolist()
+
+# =============================================================================
+# all data
+# =============================================================================
+
+X = pd.concat([
+                pd.read_feather(f) for f in tqdm(files, mininterval=60)
+               ], axis=1)
+y = utils.read_pickles('../data/label').TARGET
+
+X['nejumi'] = np.load('../feature_someone/train_nejumi.npy')
+
+if X.columns.duplicated().sum()>0:
+    raise Exception(f'duplicated!: { X.columns[X.columns.duplicated()] }')
+print('no dup :) ')
+print(f'X.shape {X.shape}')
+
+gc.collect()
+
+CAT = list( set(X.columns)&set(utils_cat.ALL))
+print(f'CAT: {CAT}')
+
+# =============================================================================
+# cv
+# =============================================================================
+dtrain = lgb.Dataset(X, y, categorical_feature=CAT, free_raw_data=False)
+gc.collect()
+
+for i in range(10):
+    seed = np.random.randint(99999)
+    print('seed:', seed)
+    param['seed'] = seed
+    ret, models = lgb.cv(param, dtrain, 9999, nfold=6,
+                         early_stopping_rounds=100, verbose_eval=50,
+                         seed=seed)
+    
+    result = f"CV auc-mean: {ret['auc-mean'][-1]} + {ret['auc-stdv'][-1]}"
+    print(result)
+    
+    utils.send_line(result)
+    imp = ex.getImp(models)
+    imp['split'] /= imp['split'].max()
+    imp['gain'] /= imp['gain'].max()
+    imp['total'] = imp['split'] + imp['gain']
+    
+    imp.sort_values('total', ascending=False, inplace=True)
+    imp.reset_index(drop=True, inplace=True)
+    
+    
+    imp.to_csv(f'LOG/imp_{__file__}-s{seed}.csv', index=False)
+
+
+#==============================================================================
+utils.end(__file__)
+utils.stop_instance()
+
