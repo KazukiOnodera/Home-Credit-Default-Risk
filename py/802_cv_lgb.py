233fac19aa65a3c42745e29d1c571aa44785132d
KazukiOnodera <luvsic02@gmail.com>
2018-08-28 16:07:05 +0900

    update

--- py/802_cv_lgb.py
+++ py/802_cv_lgb.py
@@ -110,7 +110,7 @@ for HEAD in HEADS:
     dtrain = lgb.Dataset(X, y, categorical_feature=CAT )
     gc.collect()
     
-    ret, models = lgb.cv(param, dtrain, 9999, nfolds=folds,
+    ret, models = lgb.cv(param, dtrain, 9999, folds=folds,
                          early_stopping_rounds=100, verbose_eval=50,
                          seed=SEED)
     
