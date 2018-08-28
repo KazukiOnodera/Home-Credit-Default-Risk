dfcf4411253fd8933e56832a87e216215c21df9a
Kazuki Onodera <luvsic02@gmail.com>
2018-08-28 23:15:05 +0900

    update

--- py/imp_concat.py
+++ py/imp_concat.py
@@ -13,7 +13,6 @@ li = ['/Users/Kazuki/Downloads/imp_191_imp.py.csv',
       '/Users/Kazuki/Downloads/imp_193_imp.py.csv',
       '/Users/Kazuki/Downloads/imp_194_imp.py.csv',
       '/Users/Kazuki/Downloads/imp_195_imp.py.csv',
-      '/Users/kazuki.onodera/Downloads/imp_798_imp.py.csv',
       
       ]
 
@@ -22,7 +21,8 @@ tmp = []
 for p in li:
     imp = pd.read_csv(p)
     
-    tmp += imp[imp['split']>5].feature.tolist()
+    tmp += imp[imp['split']>0][imp.feature.str.startswith('f15')].feature.tolist()
+    tmp += imp[imp['split']>0][imp.feature.str.startswith('f025')].feature.tolist()
     
 #    imp['split'] /= imp['split'].max()
 #    imp['gain'] /= imp['gain'].max()
@@ -30,11 +30,20 @@ for p in li:
 #    tmp += imp[imp['total']>0.005].feature.tolist()
 
 
-tmp += pd.read_csv('/Users/kazuki.onodera/Downloads/imp_801_imp_lgb.py-2.csv').head(1000).feature.tolist()
+#tmp += pd.read_csv('/Users/kazuki.onodera/Downloads/imp_801_imp_lgb.py-2.csv').head(1000).feature.tolist()
 
 
 tmp = sorted(set(tmp))
 df = pd.DataFrame(tmp, columns=['feature'])
 
 df.to_csv('imp_atleast_use.csv', index=False)
+
+
+imp1 = pd.read_csv('/Users/Kazuki/Downloads/imp_801_imp_lgb.py-2-1.csv').set_index('feature')
+imp2 = pd.read_csv('/Users/Kazuki/Downloads/imp_801_imp_lgb.py-2-2.csv').set_index('feature')
+
+
+tmp = (imp1.total + imp2.total).sort_values(ascending=False).to_frame().reset_index()
+
+
 
